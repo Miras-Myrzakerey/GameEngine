975da402061626bcbb219951e91dcac04cbabdfd
Miras Myrzakerey <miras.myrzakerey@nu.edu.kz>
2017-11-10 15:29:47 +0600

    UObject changes

--- GameEngine/UObject.cpp
+++ GameEngine/UObject.cpp
@@ -1,9 +1,25 @@
 #include "UObject.h"
 
-UObject::UObject()
-{
+int UObject::m_objectAmount = 0;
+
+UObject::UObject(const std::tstring& name) 
+:m_Name(name)
+, m_id(m_objectAmount)
+, m_bIsInitialized(false)
+, m_bIsPostInitialized(false)
+, m_bIsContentLoaded(false)
+, m_bIsPostContentLoaded(false)
+, m_bIsDestroyed(false)
+, m_bCanDraw(true)
+, m_bCanTick(true) 
+{	
+	++m_objectAmount;
 }
 
-UObject::~UObject()
-{
+UObject::~UObject() {
+	--m_objectAmount;
+}
+
+void UObject::Reset(){
+
 }
--- GameEngine/UObject.h
+++ GameEngine/UObject.h
@@ -1,11 +1,99 @@
 #ifndef _UOBJECT_H
 #define _UOBJECT_H
 
+//Additional Includes
+#ifndef _CONTEXT_H
+	#include "context.h"
+#endif
+
+#ifndef _STRING_H
+	#include "string.h"
+#endif
+
 class UObject
 {
 public:
-	UObject();
-	~UObject();
+	UObject(const std::tstring& name);
+	virtual ~UObject();
+
+	//void* operator new(size_t size) {};
+	//void operator delete(size_t size) {};
+
+	virtual bool Initialize() {
+		m_bIsInitialized = true;
+		return true;
+	}
+	virtual bool PostInitialize() {
+		m_bIsPostInitialized = true;
+		return true;
+	}
+	virtual bool LoadContent() {
+		m_bIsContentLoaded = true;
+		return true;
+	}
+	virtual bool PostLoadContent() {
+		m_bIsPostContentLoaded = true;
+		return true;
+	}
+
+	virtual bool Update() {
+		return true;
+	}
+	//virtual bool LateUpdate() {
+	//	return true
+	//}
+	virtual bool Draw() {
+		return true;
+	}
+	virtual bool DrawUI() {
+		return true;
+	}
+	virtual bool Shutdown() {
+		return true;	
+	}
+
+	virtual void Reset();
+
+	void SetName(const std::tstring& newName) { m_Name = newName };
+	const std::tstring& GetName() { return m_Name };
+
+
+	bool Initialized() { return m_bIsInitialized; }
+
+	bool isPostInitialized() { return m_bIsPostInitialized; }
+
+	bool isContentLoaded() { return m_bIsContentLoaded; }
+
+	bool isPostContentLoaded() {return m_bIsPostContentLoaded; }
+
+	void setCanTick(bool ct) { m_bCanTick = ct; }
+
+	bool CanTick() { return m_bCanTick; }
+
+	void setCanDraw(bool cd) { m_bCanDraw = cd; }
+
+	bool canDraw() { return m_bCanDraw; }
+
+	void Destroy() { m_bIsDestroyed = true; }
+
+	bool isDestroyed() {return m_bIsDestroyed; }
+
+protected:
+	static int m_objectAmount;
+	int m_id;
+
+	bool m_bIsInitialized;
+	bool m_bIsPostInitialized;
+	
+	bool m_bIsContentLoaded;
+	bool m_bIsPostContentLoaded;
+	
+	std::tstring m_Name;
+
+private:
+	bool m_bIsDestroyed;
+	bool m_bCanTick;
+	bool m_bCanDraw;
 };
 
 #endif
